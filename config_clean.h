9dab3a5e51d040659683a6a20663a77aa527bf88
Jesse <41927273+WorkVerbDesign@users.noreply.github.com>
2020-04-01 13:37:05 -0400

    Add files via upload

---
+++ config_clean.h
@@ -0,0 +1,100 @@
+// refresh and whatnot https://twitchtokengenerator.com/
+
+// butts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const String OAuth = "abc1234alfjiehlakfuhlaeokfuih";
+const String chanId = "yourChannelId28282";
+//oauth rerefresh: abc1234
+//https://twitchtokengenerator.com/
+
+/* need to revoke these and the previous one
+    to revoke clients you need to break the connection from
+    https://www.twitch.tv/settings/connections
+    and if you want reconnect the app to make a new one..
+    https://twitchapps.com/tmi/
+    that app sucks it needs to have a thing to individually revoke
+*/
